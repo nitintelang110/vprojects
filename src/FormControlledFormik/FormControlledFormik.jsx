98a069afd71bc25cbbb38bb764d39ec7499d267b
NITIN M. TELANG <nitintelang110@gmail.com>
2024-02-07 10:16:36 +0530

    commit

--- src/FormControlledFormik/FormControlledFormik.jsx
+++ src/FormControlledFormik/FormControlledFormik.jsx
@@ -1,55 +1,77 @@
 import React from 'react';
 import './FormControlledFormik.css';
-import { useFormik } from 'formik';
-import { YupValidation } from './YupValidation';
+import {useFormik} from 'formik';
+import {YupValidation} from './YupValidation';
 
-
+const initialValues = {
+    name: '',
+    email: '',
+    password: '',
+    cpassword: ''
+}
 
 const FormControlledFormik = () => {
 
-  //here we are using useafaormik and destructing needed formik properties
+    //here we are using useafaormik and destructing needed formik properties
 
-const {values,handleBlur,handleChange,handleSubmit,errors} = useFormik({
-  
-  initialValues:{
-    name :'',
-    email:'',
-    password:'',
-    c_password:''
-  },
-  validationSchema:{YupValidation},
-  onSubmit:(values)=>{console.log(values)},
-})
+    const {values, handleBlur, handleChange, handleSubmit, errors} = useFormik({
 
+        initialValues: initialValues,
+        validationSchema:YupValidation,
+        onSubmit: (values) => {
+            console.log(values)
+        }
+    })
 
- 
-  return (
-    <div className='c_form_container_formik'>
-      <form className='form_container' onSubmit={handleSubmit}>
+    return (
+        <div className='c_form_container_formik'>
+            <form className='form_container' onSubmit={handleSubmit}>
 
-      <label htmlFor='name' className='form_label'>Name</label>
-      <input type='text' id='name' name='name' onBlur={handleBlur}  onChange = {handleChange} value={values.name} autoComplete='off'/>
- 
-      {errors.name && <small>{errors.name}</small> }
-      
-     
-      <label htmlFor='email' className='form_label'>Email</label>
-      <input type='text' id='email' name='email' onBlur={handleBlur}  onChange = {handleChange} value={values.email}  autoComplete='off'/>
-      {errors.email && <small>{errors.email}</small> }
-     
-      <label htmlFor='password' className='form_label'>Password</label>
-      <input type='password' id='password' name='password' onBlur={handleBlur}  onChange = {handleChange} value={values.password} autoComplete='off' />
-      {errors.password && <small>{errors.password}</small> }
-     
-      <label htmlFor='c_password' className='form_label'>Confirm Password</label>
-      <input type='text' id='c_password' name='c_password' onBlur={handleBlur}  onChange = {handleChange} value={values.c_password}  autoComplete='off'/>
-      {errors.c_password && <small>{errors.c_password}</small> }
-     
-      <button type='submit'>Submit</button>
-     
-      </form>
-      </div>
-  )
+                <label htmlFor='name' className='form_label'>Name</label>
+                <input
+                    type='text'
+                    id='name'
+                    name='name'
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={values.name}
+                    autoComplete='off'/> {errors.name && <small style ={{color:"red"}}>{errors.name}</small>}
+
+                <label htmlFor='email' className='form_label'>Email</label>
+                <input
+                    type='text'
+                    id='email'
+                    name='email'
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={values.email}
+                    autoComplete='off'/> {errors.email && <small style ={{color:"red"}}>{errors.email}</small>}
+
+                <label htmlFor='password' className='form_label'>Password</label>
+                <input
+                    type='text'
+                    id='password'
+                    name='password'
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={values.password}
+                    autoComplete='off'/> {errors.password && <small style ={{color:"red"}}>{errors.password}</small>}
+
+                <label htmlFor='cpassword' className='form_label'>Confirm Password</label>
+                <input
+                    type='text'
+                    id='cpassword'
+                    name='cpassword'
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={values.cpassword}
+                    autoComplete='off'/> {errors.cpassword && <small style ={{color:"red"}}>{errors.cpassword}</small>}
+
+                <button type='submit'>Submit</button>
+
+            </form>
+        </div>
+    )
 }
 
 export default FormControlledFormik
--- src/FormControlledFormik/YupValidation.jsx
+++ src/FormControlledFormik/YupValidation.jsx
@@ -1,11 +1,11 @@
 
 import * as Yup from 'yup';
 
-export const YupValidation = {
+export const YupValidation = Yup.object({
  
     name:Yup.string().min(3).required("Please Enter Name"),
-    eamil:Yup.string().email("Please Enter Valid Email").required("Please Enter Email"),
-    password:Yup.string().min(5).required("Please Enter Password"),
-    c_password:Yup.string().oneOf([Yup.ref("password")] , "Password Not Matched"),
-}
+    email:Yup.string().matches(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,"Enter Valid Email").email("Please Enter Valid Email").required("Please Enter Email"),
+    password:Yup.string().min(5).matches( /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&^])[A-Za-z\d@.#$!%*?&]{8,15}$/,"password includes one Capital letter,one Small letter,one Special Character and numbers").required("Please Enter Password"),
+    cpassword:Yup.string().oneOf([Yup.ref("password")] , "Password Not Matched"),
+}) 
 
